43d6005f84b37c500126f1caf2f5fb2f86a94f83
Gabriel <quontas@gmail.com>
2021-02-21 20:49:37 -0500

    Temporary WIP.

--- client/components/rotationReader.ts
+++ client/components/rotationReader.ts
@@ -1,16 +1,25 @@
-import {registerComponent} from 'aframe';
+import {Entity, registerComponent} from 'aframe';
+import { Object3D, Vector3 } from 'three';
+
+const getChildEntityPosition = (childId: string): Vector3 => {
+  const childEntity = document.querySelector(`#${childId}`);
+  const childEntityPosition = childEntity?.object3D.position;
+  const parentEntityPosition = (childEntity?.parentElement as Entity).object3D.position;
+  return new Vector3().copy(childEntityPosition).add(parentEntityPosition);
+}
 
 export const rotationReaderComponent = registerComponent('rotation-reader', {
-  /**
-   * We use IIFE (immediately-invoked function expression) to only allocate one
-   * vector or euler and not re-create on every tick to save memory.
-   */
-  tick: (function () {
-    return function () {
-      // using "this" does not support getting object3D, so we use querySelector instead
-      // const camRotation = document.querySelector('a-camera').object3D.rotation;
-      // const {x, y, z} = camRotation;
-      // console.log({x}, {y}, {z});
-    };
-  })(),
+  tick: function () {
+    const canvas: HTMLCanvasElement = document.getElementById('global-setup') as HTMLCanvasElement;
+    const ctx = canvas.getContext('2d');
+    ctx?.beginPath();
+    ctx?.arc(10, 10, 10, 0, 2 * Math.PI);
+    ctx?.stroke();
+
+    const globalJurorAPosition = getChildEntityPosition('juror-a');
+    const globalJurorBPosition = getChildEntityPosition('juror-b');
+    const globalJurorCPosition = getChildEntityPosition('juror-c');
+    const globalJuryForemanPosition = getChildEntityPosition('jury-foreman');
+    // console.log({globalJurorAPosition, globalJurorBPosition, globalJurorCPosition, globalJuryForemanPosition})
+  },
 });
