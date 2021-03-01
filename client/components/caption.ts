a4fbc74477dc22808c43f5c97d3b127ea7cde12b
Gabriel Britain <quontas@gmail.com>
2021-03-01 12:37:16 -0500

    Added fading triangles and keyboard controls.

--- client/components/caption.ts
+++ client/components/caption.ts
@@ -1,13 +1,8 @@
 import {Entity, registerComponent} from 'aframe';
 import {Vector3} from 'three';
 
-const getChildEntityPosition = (childId: string): Vector3 => {
-  const childEntity = document.querySelector(`#${childId}`);
-  const childEntityPosition = childEntity?.object3D.position;
-  const parentEntityPosition = (childEntity?.parentElement as Entity).object3D
-    .position;
-  return new Vector3().copy(childEntityPosition).add(parentEntityPosition);
-};
+const INACTIVE_INDICATOR_COLOR = '#FFD700';
+const ACTIVE_INDICATOR_COLOR = '#FF0000';
 
 const setSpeakerAsActive = (speaker: string) => {
   const captionEl = document.querySelector('a-text#caption');
@@ -30,27 +25,14 @@ const setSpeakerAsActive = (speaker: string) => {
   );
 };
 
-const displayArrowOnPhone = (
-  speakerPosition: Vector3,
-  phonePosition: Vector3
-) => {
-  const phoneArrow = new Vector3();
-  phoneArrow.setX(speakerPosition.x - phonePosition.x);
-  phoneArrow.setY(0);
-  phoneArrow.setZ(speakerPosition.z - phonePosition.z);
-  console.log('Before normalization', phoneArrow);
-  const normalizedPhoneArrow = phoneArrow.normalize();
-  const scaledPhoneArrow = normalizedPhoneArrow.multiplyScalar(0.3);
-  console.log('After normalization and scaling', scaledPhoneArrow);
-  const startPoint = new Vector3().copy(phonePosition);
-  const endPoint = new Vector3().copy(phonePosition).add(scaledPhoneArrow);
-  const thickLine = document.createElement('a-entity');
-  thickLine.setAttribute(
-    'meshline',
-    `lineWidth: 20; path:${startPoint.x} ${startPoint.y} ${startPoint.z},${endPoint.x} ${endPoint.y} ${endPoint.z}; color: #FFD700`
+const displayTriangleOnPhone = (speakerId: string, isActive: boolean) => {
+  const speakerTriangle = document.querySelector(
+    `a-triangle[speaker-id=${speakerId}]`
   );
-  thickLine.setAttribute('class', 'thickline');
-  document.querySelector('a-scene').appendChild(thickLine);
+  if (isActive) {
+    speakerTriangle.setAttribute('color', ACTIVE_INDICATOR_COLOR);
+  }
+  speakerTriangle.emit('fade');
 };
 
 export const captionComponent = registerComponent('caption', {
@@ -78,23 +60,24 @@ export const captionComponent = registerComponent('caption', {
     if (!this.data.speaker || !this.data.activeTarget) {
       return;
     }
+    const speakerIsActive = this.data.speaker === this.data.activeTarget;
     if (this.data.speaker !== '') {
       console.log('speaker is', this.data.speaker);
-      const phonePosition = document.querySelector('#smartphone').object3D
-        .position;
-      const speakerPosition = getChildEntityPosition(this.data.speaker);
-      document
-        .querySelectorAll('.thickline')
-        .forEach(elem => elem.parentNode?.removeChild(elem));
-      displayArrowOnPhone(speakerPosition, phonePosition);
+      document.querySelectorAll('a-triangle').forEach(elem => {
+        elem.setAttribute('color', INACTIVE_INDICATOR_COLOR);
+        elem.setAttribute('material', 'opacity', 0);
+      });
+      displayTriangleOnPhone(
+        this.data.speaker,
+        speakerIsActive
+      );
     }
     let opacity: number;
-    const cursorIsOnSpeaker = this.data.speaker === this.data.activeTarget;
     if (this.data.ambientCaption) {
       // If this is an ambient captioning element, we want it only to appear when not on the correct speaker.
-      opacity = !cursorIsOnSpeaker ? 1 : 0;
+      opacity = !speakerIsActive ? 1 : 0;
     } else {
-      opacity = cursorIsOnSpeaker ? 1 : 0;
+      opacity = speakerIsActive ? 1 : 0;
     }
     this.el.setAttribute('opacity', opacity);
   },
