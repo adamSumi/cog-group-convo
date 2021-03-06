dfb2b68103d78499d553fb313bcb16d86e9ff1d1
Gabriel <quontas@gmail.com>
2021-03-06 12:44:44 -0600

    Performance profiling.

--- client/components/indicator.ts
+++ client/components/indicator.ts
@@ -7,15 +7,19 @@ const ACTIVE_INDICATOR_COLOR = '#FF0000';
 export const indicatorComponent = registerComponent('indicator', {
   schema: {speakerId: {type: 'string'}},
 
+  init: function () {
+    // @ts-ignore
+    this.speakerWorldPosition = document
+      .querySelector(`#${this.data.speakerId}`)
+      .object3D.getWorldPosition(new Vector3());
+  },
   rotateIndicator: function (indicator: Entity, position: Vector3) {
     indicator.object3D.lookAt(position);
     indicator.object3D.rotateX(90);
   },
 
   tick: function () {
-    const speakerWorldPosition = document
-      .querySelector(`#${this.data.speakerId}`)
-      .object3D.getWorldPosition(new Vector3());
-    this.rotateIndicator(this.el, speakerWorldPosition);
+    // @ts-ignore
+    this.rotateIndicator(this.el, this.speakerWorldPosition);
   },
 });
