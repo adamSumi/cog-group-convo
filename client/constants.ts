9ecacdf7ecda02ab5336a9e9762461b2fc154ee2
Gabriel Britain <quontas@gmail.com>
2021-03-27 17:16:50 -0400

    Overhauled for multi-monitor purposes.

--- client/constants.ts
+++
@@ -1,6 +0,0 @@
-export const PADDING = 0.025;
-export const MAX_HEIGHT = 0.25;
-export const MAX_WIDTH = 1.5;
-export const MAX_CHARACTER_COUNT = 40;
-export const MESSAGE_EXPIRATION_TIME_MILLIS = 9000;
-export const VIDEO_PLAYBACK_NAME = 'video-playback';
