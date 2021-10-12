4434dd0c06f683b4687dd6c6cf874c423698ba75
Gabriel Britain <quontas@gmail.com>
2021-10-12 17:01:25 -0400

    Observables working as expected, but duplicates being dispatched.

--- server.py
+++ server.py
@@ -1,70 +1,79 @@
 import json
 import logging
-import queue
+import multiprocessing
+import time
+import datetime
+import random
 import socket
-import sys
-import threading
+from typing import Dict, Optional, Tuple, List
+
+import rx
+from rx import operators as ops
+from rx.core.typing import Observable, Observer, Scheduler
+from rx.scheduler.threadpoolscheduler import ThreadPoolScheduler
 
 import captions
 from common import BYTEORDER, HEADER_SIZE, HOST, PORT
 
 EXPECTED_CHARACTER = ""
 
+logging.basicConfig(level=logging.DEBUG)
 
-def dispatch_captions(captions_queue: queue.Queue) -> None:
-    logger = logging.basicConfig(
-        stream=sys.stdout, name="dispatch_captions", level=logging.INFO
+random_juror = lambda: random.choice(
+    ["juror-a", "juror-b", "juror-c", "jury-foreman", None]
+)
+
+
+def socket_transmission(
+    caption: Dict[str, str], juror: Optional[str], conn: socket.socket
+) -> None:
+    message = json.dumps(caption).encode("utf-8")
+    message_len = len(message)
+    message_len_bytes = message_len.to_bytes(HEADER_SIZE, BYTEORDER)
+    conn.sendall(message_len_bytes)
+    conn.sendall(message)
+
+
+def serial_monitor(observer: Observer, scheduler: Scheduler) -> str:
+    while True:
+        observer.on_next(random_juror())
+        time.sleep(1)
+
+
+def build_delayed_caption_obs(caption: Dict[str, str]) -> Observable:
+    return rx.just(caption).pipe(
+        ops.delay(datetime.timedelta(milliseconds=random.randint(0, 1000)))
     )
-    for caption in captions.CAPTIONS:
-        logger.debug(f"Putting new caption into queue: {caption}")
-        captions_queue.put(caption)
-        # time.sleep(random.randrange(0, 10))
-    logger.info(f"Completed caption transmission, putting None in queue to terminate.")
-    captions_queue.put(None)
-
-
-def socket_transmission(captions_queue: queue.Queue, conn: socket.socket) -> None:
-    logger = logging.basicConfig(
-        stream=sys.stdout, name="socket_transmission", level=logging.INFO
-    )
-    while caption := captions_queue.get():
-        logger.debug(f"Received a message from input queue. Message is: {caption}")
-        message = json.dumps(caption).encode("utf-8")
-        message_len = len(message)
-        message_len_bytes = message_len.to_bytes(HEADER_SIZE, BYTEORDER)
-        logger.debug(f"Sending message length to socket: {message_len}")
-        conn.sendall(message_len_bytes)
-        logger.debug(f"Sending message in UTF-8 format to socket")
-        conn.sendall(message)
-        captions_queue.task_done()
-    logger.info(f"Received None from queue, ending thread.")
-    captions_queue.task_done()
 
 
 def main(host: str = HOST, port: int = PORT) -> None:
-    captions_queue = queue.Queue()
+    # captions_queue = queue.Queue()
+    scheduler = ThreadPoolScheduler(multiprocessing.cpu_count())
+    captions_observable: Observable = rx.concat_with_iterable(
+        build_delayed_caption_obs(caption) for caption in captions.CAPTIONS
+    )
+    serial_monitor_observable = rx.create(serial_monitor).pipe(
+        ops.subscribe_on(scheduler),
+        ops.distinct_until_changed()
+    )
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         sock.bind((host, port))
         sock.listen()
         conn, addr = sock.accept()
         logging.info(f"Socket connection received from: {addr}")
-        with conn:
-            ready = input("Press ENTER to begin the experiment.")
-            while ready != EXPECTED_CHARACTER:
-                ready = input(
-                    "Invalid character received. Press ENTER to begin the experiment."
-                )
-            t1 = threading.Thread(target=dispatch_captions, args=(captions_queue,))
-            t2 = threading.Thread(
-                target=socket_transmission, args=(captions_queue, conn)
+        ready = input("Press ENTER to begin the experiment.")
+        while ready != EXPECTED_CHARACTER:
+            ready = input(
+                "Invalid character received. Press ENTER to begin the experiment."
             )
-            logging.debug("Starting process_captions_with_serial thread")
-            t1.start()
-            logging.debug("Starting socket_transmission thread")
-            t2.start()
-
-            captions_queue.join()
-            logging.info("All tasks put in the queue have been completed.")
+        captions_and_serial_observable = rx.combine_latest(
+            captions_observable,
+            serial_monitor_observable,
+        )
+        captions_and_serial_observable.subscribe(
+            lambda x: socket_transmission(x[0], x[1], conn)
+        )
 
 
 if __name__ == "__main__":
