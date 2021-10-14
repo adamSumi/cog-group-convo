1b5ed7503ddbf4c4a75e876f338664175bb11d33
Gabriel Britain <quontas@gmail.com>
2021-10-14 11:40:08 -0400

    Made client a bit more customizable

--- client.py
+++ client.py
@@ -6,7 +6,9 @@ from pprint import pprint
 from common import BYTEORDER, HEADER_SIZE, HOST, PORT
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
+    host = input("IP address?")
+    port = input("Port?")
+    s.connect((host, int(port)))
     while msg_len_bytes := s.recv(HEADER_SIZE):
         data = s.recv(int.from_bytes(msg_len_bytes, BYTEORDER))
         data_as_str = data.decode("utf-8")
