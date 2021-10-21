285edf3085e0b41c69d4ebbb96a30982e338fcd6
Gabriel Britain <quontas@gmail.com>
2021-10-21 12:04:35 -0400

    Experimenting with graceful closure.

--- dummy.py
+++ dummy.py
@@ -1,19 +1,34 @@
 import random
+from typing import Callable, Literal
+import multiprocessing
+
 import rx
-from rx.core.typing import Observer, Scheduler
 from rx import operators as ops
+from rx.core.typing import Observer, Scheduler, Observable
+from rx.internal.utils import infinite
 from rx.scheduler.threadpoolscheduler import ThreadPoolScheduler
 
+random_juror: Callable[
+    [], Literal["juror-a", "juror-b", "juror-c", "jury-foreman", None]
+] = lambda: random.choice(["juror-a", "juror-b", "juror-c", "jury-foreman", None])
 
-def random_output(observer: Observer, scheduler: Scheduler) -> None:
+
+def infinite_observable(observer: Observer, scheduler: Scheduler) -> None:
     while True:
-        observer.on_next(random.randint(0, 1000))
-        # print("In loop")
+        val = random_juror()
+        print(val)
+        observer.on_next(val)
 
 
 scheduler = ThreadPoolScheduler()
-random_obs = rx.create(random_output).pipe(ops.subscribe_on(scheduler))
-event_obs = rx.from_list([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-rx.combine_latest(event_obs, random_obs).subscribe(
-    on_next=lambda x: print(f"Received {x}")
-)
+infinite_obs: Observable[
+    Literal["juror-a", "juror-b", "juror-c", "jury-foreman", None]
+] = rx.create(infinite_observable).pipe(ops.subscribe_on(scheduler))
+event_obs: Observable[int] = rx.from_list([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+
+rx.combine_latest(infinite_obs, event_obs).pipe(
+    # ops.take_until(event_obs.pipe(ops.is_empty()))
+).subscribe(on_next=print, on_completed=lambda x: print("All done!"))
+
+while True:
+    pass
