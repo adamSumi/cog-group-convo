788fccde47141126cf5129e6f8f90275116bb248
Gabriel Britain <gabriel.britain@protonmail.com>
2022-03-04 21:41:32 -0500

    Adding captions back in, working on writing. Must be writing to the wrong address, because Android not receiving.

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+#define GL_SILENCE_DEPRECATION
+
 #include <cstdlib>
 #include <netinet/in.h>
 #include <arpa/inet.h>
@@ -6,14 +8,17 @@
 #include <ifaddrs.h>
 #include <unistd.h>
 #include "orientation_message_generated.h"
+#include "caption_message_generated.h"
 #include <thread>
 #include <numeric>
 #include <deque>
 #include <cmath>
+#include <nlohmann/json.hpp>
 
 #ifdef __APPLE_CC__
 
 #include <GL/glut.h>
+#include <fstream>
 
 #else
 #include <GL/glut.h>
@@ -29,6 +34,8 @@ cog::OrientationMessage const *current_orientation = nullptr;
 std::mutex orientation_mutex;
 std::deque<float> orientation_buffer;
 
+std::mutex socket_mutex;
+
 /**
  * Prints a QR code to the console. The QR code's contents are formatted as follows:
  * "<MACHINE_IP_ADDR>:<PORT> <PRESENTATION_METHOD>"
@@ -39,6 +46,7 @@ void print_connection_qr(const int *presentation_method) {
     struct sockaddr_in *sa;
     char *addr;
     getifaddrs(&ifap);
+    // This is some witchcraft from StackOverflow
     for (ifa = ifap; ifa; ifa = ifa->ifa_next) {
         if (ifa->ifa_addr && ifa->ifa_addr->sa_family == AF_INET) {
             sa = (struct sockaddr_in *) ifa->ifa_addr;
@@ -61,14 +69,22 @@ void print_connection_qr(const int *presentation_method) {
     freeifaddrs(ifap);
 }
 
+/**
+ * Reads bytes from the provided socket, converts those bytes into an OrientationMessage,
+ * and adds the azimuth of that OrientationMessage to the orientation buffer, which does filtering on the readings.
+ * @param socket
+ * @param client_address
+ */
 void read_orientation(int socket, sockaddr_in client_address) {
     size_t len, num_bytes_read;
     std::array<char, 1024> buffer{};
     len = sizeof(client_address);
 
+    socket_mutex.lock();
     num_bytes_read = recvfrom(socket, buffer.data(), buffer.size(),
                               MSG_WAITALL, (struct sockaddr *) &client_address,
                               reinterpret_cast<socklen_t *>(&len));
+    socket_mutex.unlock();
     while (num_bytes_read != -1) {
         orientation_mutex.lock();
         if (orientation_buffer.size() == MOVING_AVG_SIZE) {
@@ -77,9 +93,48 @@ void read_orientation(int socket, sockaddr_in client_address) {
         current_orientation = cog::GetOrientationMessage(buffer.data());
         orientation_buffer.push_back(current_orientation->azimuth());
         orientation_mutex.unlock();
+        socket_mutex.lock();
         num_bytes_read = recvfrom(socket, buffer.data(), buffer.size(),
                                   MSG_WAITALL, (struct sockaddr *) &client_address,
                                   reinterpret_cast<socklen_t *>(&len));
+        socket_mutex.unlock();
+    }
+}
+
+
+void stream_captions(int socket, const sockaddr *client_address, const nlohmann::json &captions_json) {
+    std::cout << "Starting to stream captions..." << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(captions_json.at(0)["delay"]));
+    flatbuffers::FlatBufferBuilder builder(1024);
+    for (auto i = 1; i < captions_json.size(); ++i) {
+        int delay = (int) captions_json.at(i)["delay"] - (int) captions_json.at(i - 1)["delay"];
+        std::this_thread::sleep_for(std::chrono::milliseconds(delay));
+        auto caption_text = builder.CreateString(captions_json.at(i)["text"].get<std::string>());
+        cog::Juror juror;
+        const auto serialized_speaker_id = captions_json.at(i)["speaker_id"].get<std::string>();
+
+        if (serialized_speaker_id == "juror-a") {
+            juror = cog::Juror_JurorA;
+        } else if (serialized_speaker_id == "juror-b") {
+            juror = cog::Juror_JurorB;
+        } else if (serialized_speaker_id == "juror-c") {
+            juror = cog::Juror_JurorC;
+        } else if (serialized_speaker_id == "jury-foreman") {
+            juror = cog::Juror_JuryForeman;
+        } else {
+            throw;
+        }
+        auto message_id = captions_json.at(i)["message_id"].get<int32_t>();
+        auto chunk_id = captions_json.at(i)["message_id"].get<int32_t>();
+
+        auto caption_message = cog::CreateCaptionMessage(builder, caption_text, juror, juror, message_id, chunk_id);
+        builder.Finish(caption_message);
+        socket_mutex.lock();
+        unsigned int len = sizeof(client_address);
+        std::cout << captions_json.at(i)["text"].get<std::string>() << std::endl;
+        sendto(socket, builder.GetBufferPointer(), builder.GetSize(), 0, client_address,
+               reinterpret_cast<socklen_t>(len));
+        socket_mutex.unlock();
     }
 }
 
@@ -144,6 +199,10 @@ int main(int argc, char **argv) {
     std::cout << "Using presentation method: " << presentation_method << std::endl;
     std::cout << "Playing video section: " << video_section << std::endl;
 
+    nlohmann::json json;
+    std::ifstream captions_file("captions/merged_captions.1.json");
+    captions_file >> json;
+
     print_connection_qr(&presentation_method);
     int sockfd;
     struct sockaddr_in servaddr{}, cliaddr{};
@@ -168,7 +227,6 @@ int main(int argc, char **argv) {
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
-
     std::thread read_orientation_thread(read_orientation, sockfd, cliaddr);
     while (true) {
         orientation_mutex.lock();
@@ -179,6 +237,7 @@ int main(int argc, char **argv) {
         }
         orientation_mutex.unlock();
     }
+//    std::thread stream_captions_thread(stream_captions, sockfd, reinterpret_cast<const sockaddr *>(&cliaddr), json);
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
 
