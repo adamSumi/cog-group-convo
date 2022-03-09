3d5d54d392cde3656f653418e9a087313b515676
Gabriel Britain <gabriel.britain@protonmail.com>
2022-03-09 14:16:57 -0500

    Improved documentation on main.cpp, also enabled changing videos and captions based on selected video section.

--- src/main.cpp
+++ src/main.cpp
@@ -8,30 +8,31 @@
 #include <fstream>
 #include <cstdlib>
 #include <vlc/vlc.h>
-#include <cstdint>
-#include <cmath>
 
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_mutex.h>
 
 #define PORT 65432
 
-#define WIDTH 640
-#define HEIGHT 480
+#define WIDTH 640 // How wide do we want the created window to be?
+#define HEIGHT 480 // How tall do we want the created window to be?
 
-//#include <format>
-
-#define WINDOW_TITLE    "SDL2"
+#define WINDOW_TITLE "Four Angry Men"
 
 #define REGISTERED_GRAPHICS 1
 #define NONREGISTERED_GRAPHICS 2
 
 
-// VLC prepares to render a video frame.
+/**
+ * This function is called prior to VLC rendering a video frame.
+ * What we do here is lock our mutex (preventing other threads from touching the texture), and lock the texture from being
+ * modified by other threads. This allows VLC to render to the texture peacefully, without data races.
+ * @param data A pointer to data that would be useful for whatever we want to do in this function (in this case, AppContext, for mutex/texture access)
+ * @param p_pixels An array of pixels representing the image, stored as concatenated rows
+ * @return nullptr.
+ */
 static void *lock(void *data, void **p_pixels) {
-
     auto *c = (AppContext *) data;
-
     int pitch;
     SDL_LockMutex(c->mutex);
     SDL_LockTexture(c->texture, nullptr, p_pixels, &pitch);
@@ -39,43 +40,39 @@ static void *lock(void *data, void **p_pixels) {
     return nullptr; // Picture identifier, not needed here.
 }
 
-// VLC just rendered a video frame.
-static void unlock(void *data, void *id, void *const *p_pixels) {
+/**
+ * This function is called after VLC renders a video frame. Once VLC is done writing a frame, we want to immediately overlay the captions on top of the frame, according to the presentation method provided, and render the frame. Once rendered, that caption, we unlock the mutex and texture for other threads to access.
+ * @param data A pointer to data that would be useful for whatever we want to do in this function (in this case, AppContext, for presentation method/mutex/texture access)
+ * @param id Honestly? Not really sure what this parameter is, but I haven't needed it. It's here to comply with the function signature expected by VLC.
+ * @param p_pixels An array of pixels representing the image, stored as concatenated rows
+ */
+static void unlock(void *data, [[maybe_unused]] void *id, [[maybe_unused]] void *const *p_pixels) {
 
     auto *c = (AppContext *) data;
 
-    auto *pixels = (uint16_t *) *p_pixels;
-
-    // We can also render stuff.
-
+    // Based on the presentation method selected by the researcher, we want to render captions in different ways.
     switch (c->presentation_method) {
         case NONREGISTERED_GRAPHICS:
+            // Non-registered graphics follow the user's head orientation around the screen
             render_nonregistered_captions(c);
             break;
         case REGISTERED_GRAPHICS:
+            // Registered graphics remain stationary in space
         default:
             std::cout << "Unknown method received: " << c->presentation_method << std::endl;
             break;
     }
     SDL_RenderPresent(c->renderer);
-
-//    int x, y;
-//    for (y = 10; y < 40; y++) {
-//        for (x = 10; x < 40; x++) {
-//            if (x < 13 || y < 13 || x > 36 || y > 36) {
-//                pixels[y * WIDTH + x] = 0xffff;
-//            } else {
-//                // RV16 = 5+6+5 pixels per color, BGR.
-//                pixels[y * WIDTH + x] = 0x02ff;
-//            }
-//        }
-//    }
-
     SDL_UnlockTexture(c->texture);
     SDL_UnlockMutex(c->mutex);
 }
 
-// VLC wants to display a video frame.
+/**
+ * This function is called when VLC wants to render a frame. We create a blank rectangle texture,
+ * which VLC will render to outside of this program.
+ * @param data A pointer to data that would be useful for whatever we want to do in this function (in this case, AppContext, for presentation method/mutex/texture access)
+ * @param id Honestly? Not really sure what this parameter is, but I haven't needed it. It's here to comply with the function signature expected by VLC.
+ */
 static void display(void *data, void *id) {
 
     auto *c = (AppContext *) data;
@@ -89,44 +86,52 @@ static void display(void *data, void *id) {
     SDL_SetRenderDrawColor(c->renderer, 0, 0, 0, 255);
     SDL_RenderClear(c->renderer);
     SDL_RenderCopy(c->renderer, c->texture, nullptr, &rect);
-//    SDL_RenderPresent(c->renderer);
 }
 
 int main(int argc, char *argv[]) {
-    const auto[video_section, presentation_method, fg, bg, path_to_font, font_size] = parse_arguments(
-            argc, argv);
+    // Get command-line arguments, which will be used for configuring how captions are rendered.
+    const auto
+    [
+    video_section, // Which video section will we be rendering?
+    presentation_method, // How will we be presenting captions?
+    fg, // What color will the text be? RGBA format
+    bg, // What color will the background behind the text be? RGBA format
+    path_to_font, // Where's the font located?
+    font_size // How big will the font be?
+    ] = parse_arguments(argc, argv);
 
     std::cout << "Using presentation method: " << presentation_method << std::endl;
     std::cout << "Playing video section: " << video_section << std::endl;
+
+    // Print the address of this server, and which presentation method we're going to be using.
+    // This QR code will be scanned by the HWD so that it can connect to our server.
     print_connection_qr(presentation_method, PORT);
+    // Now, wait for the connection.
     auto[socket, cliaddr] = connect_to_client(PORT);
 
-    libvlc_instance_t *libvlc;
-    libvlc_media_t *m;
-    libvlc_media_player_t *mp;
-    char const *vlc_argv[] = {
-            "--no-audio", // Don't play audio.
-            "--no-xlib", // Don't use Xlib.
-    };
-    int vlc_argc = sizeof(vlc_argv) / sizeof(*vlc_argv);
-    SDL_Event event;
-    int done = 0, action, pause = 0;
-
+    // Let's start building our application context. This is basically a struct that stores pointers to
+    // important mutexes, buffers, and variables.
     struct AppContext app_context{};
     app_context.presentation_method = presentation_method;
 
-    if (argc < 2) {
-        printf("Usage: %s <filename>\n", argv[0]);
-        return EXIT_FAILURE;
-    }
+    // Let's initialize SDL
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         printf("SDL could not initialize! SDL Error: %s\n", SDL_GetError());
         return 1;
     }
+    // And initialize SDL_ttf, which will render text on our video frames.
     if (TTF_Init() == -1) {
         printf("[ERROR] TTF_Init() Failed with: %s\n", TTF_GetError());
         exit(2);
     }
+
+    TTF_Font *font = TTF_OpenFont(path_to_font.c_str(), font_size);
+    app_context.font = font;
+
+    SDL_Color foreground_color = {fg.at(0), fg.at(1), fg.at(2), fg.at(3)};
+    SDL_Color background_color = {bg.at(0), bg.at(1), bg.at(2), bg.at(3)};
+    app_context.foreground_color = &foreground_color;
+    app_context.background_color = &background_color;
 
     auto window = SDL_CreateWindow(WINDOW_TITLE, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH,
                                    HEIGHT, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
@@ -142,6 +147,17 @@ int main(int argc, char *argv[]) {
     }
     app_context.mutex = SDL_CreateMutex();
 
+
+    // Now that we've configured our app context, let's get ready to boot up VLC.
+    libvlc_instance_t *libvlc;
+    libvlc_media_t *m;
+    libvlc_media_player_t *mp;
+    char const *vlc_argv[] = {
+            "--no-audio", // Don't play audio.
+            "--no-xlib", // Don't use Xlib.
+    };
+    int vlc_argc = sizeof(vlc_argv) / sizeof(*vlc_argv);
+    int done = 0, action, pause = 0;
     // If you don't have this variable set you must have plugins directory
     // with the executable or libvlc_new() will not work!
     printf("VLC_PLUGIN_PATH=%s\n", getenv("VLC_PLUGIN_PATH"));
@@ -149,22 +165,19 @@ int main(int argc, char *argv[]) {
     // Initialise libVLC.
     libvlc = libvlc_new(vlc_argc, vlc_argv);
     if (nullptr == libvlc) {
-        printf("LibVLC initialization failure");
+        printf("LibVLC initialization failure. If on MacOS, make sure that VLC_PLUGIN_PATH is set to the path of the PARENT of the VLC plugin folder");
         return EXIT_FAILURE;
     }
 
-    m = libvlc_media_new_path(libvlc, "resources/videos/juror-a.mp4");
+    // Let's load the video that we're going to play on VLC
+    std::ostringstream os;
+    os << "resources/videos/juror-a." << video_section << ".mp4";
+    std::string video_path = os.str();
+    m = libvlc_media_new_path(libvlc, video_path.c_str());
     mp = libvlc_media_player_new_from_media(m);
     libvlc_media_release(m);
-
-    TTF_Font *font = TTF_OpenFont(path_to_font.c_str(), font_size);
-    app_context.font = font;
-
-    SDL_Color foreground_color = {fg.at(0), fg.at(1), fg.at(2), fg.at(3)};
-    SDL_Color background_color = {bg.at(0), bg.at(1), bg.at(2), bg.at(3)};
-    app_context.foreground_color = &foreground_color;
-    app_context.background_color = &background_color;
-
+    libvlc_video_set_callbacks(mp, lock, unlock, display, &app_context);
+    libvlc_video_set_format(mp, "RV16", WIDTH, HEIGHT, WIDTH * 2);
 
     std::mutex azimuth_mutex;
     app_context.azimuth_mutex = &azimuth_mutex;
@@ -175,18 +188,24 @@ int main(int argc, char *argv[]) {
                                         &azimuth_buffer);
 
     nlohmann::json json;
-    std::ifstream captions_file("resources/captions/merged_captions.1.json");
+    os.str("");
+    os.clear();
+    os << "resources/captions/merged_captions." << video_section << ".json";
+    std::string captions_path = os.str();
+    std::cout << "Captions path = " << captions_path << std::endl;
+    std::ifstream captions_file(captions_path.c_str());
     captions_file >> json;
     auto caption_model = CaptionModel(true);
     app_context.caption_model = &caption_model;
 
-    while (azimuth_buffer.empty()) {
+    // Wait for data to start getting transmitted from the phone
+    // before we start playing our video on VLC and rendering captions.
+    while (azimuth_buffer.size() < MOVING_AVG_SIZE) {
     }
-    libvlc_video_set_callbacks(mp, lock, unlock, display, &app_context);
-    libvlc_video_set_format(mp, "RV16", WIDTH, HEIGHT, WIDTH * 2);
     libvlc_media_player_play(mp);
-
     std::thread play_captions_thread(play_captions, &json, &caption_model);
+
+    SDL_Event event;
     // Main loop.
     while (!done) {
 
@@ -194,7 +213,6 @@ int main(int argc, char *argv[]) {
 
         // Keys: enter (fullscreen), space (pause), escape (quit).
         while (SDL_PollEvent(&event)) {
-
             switch (event.type) {
                 case SDL_QUIT:
                     done = 1;
@@ -210,13 +228,9 @@ int main(int argc, char *argv[]) {
             case SDLK_q:
                 done = 1;
                 break;
-            case ' ':
-                printf("Pause toggle.\n");
-                pause = !pause;
+            default:
                 break;
         }
-
-        if (!pause) { app_context.n++; }
 
         SDL_Delay(1000 / 10);
     }
