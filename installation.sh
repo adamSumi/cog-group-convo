52e99faf24f125ad0a41f76d83141cb509e230ca
Gabriel Britain <gabriel.britain@protonmail.com>
2022-03-21 10:12:20 -0400

    Better installation script

--- installation.sh
+++ installation.sh
@@ -1,26 +1,16 @@
-cd ~/Downloads/ || exit
-pwd
-git clone --depth 1 https://github.com/libsdl-org/SDL.git
-cd SDL || exit
-mkdir build
-cd build || exit
-../configure
-make
-echo "Please enter your sudo password."
-sudo make install
-git clone --depth 1 https://github.com/libsdl-org/SDL_ttf.git
-cd SDL_ttf || exit
-mkdir build
-cd build || exit
-../configure
-make
-echo "Please enter your sudo password."
-sudo make install
-git clone --depth 1 https://github.com/libsdl-org/SDL_image.git
-cd SDL_image || exit
-mkdir build
-cd build || exit
-../configure
-make
-echo "Please enter your sudo password."
-sudo make install
+function clone_and_install() {
+    cd ~/Downloads/ || exit
+    pwd
+    git clone --depth 1 "$1" "$2"
+    cd $2 || exit
+    mkdir build
+    cd build || exit
+    ../configure
+    make
+    echo "Please enter your sudo password."
+    sudo make install
+}
+
+clone_and_install https://github.com/libsdl-org/SDL.git SDL
+clone_and_install https://github.com/libsdl-org/SDL_ttf.git SDL_ttf
+clone_and_install https://github.com/libsdl-org/SDL_image.git SDL_image
