33ea41df14df19b68fd10eef0c069e62af25648c
Gabriel Britain <gabriel.britain@protonmail.com>
2022-03-22 15:56:31 -0500

    Improved splitting speed, and play/paused vlc so that the window actually renders.

--- resources/videos/split.sh
+++ resources/videos/split.sh
@@ -1,4 +1,4 @@
-ffmpeg -i main.mp4 -ss 00:00:00 -t 00:02:30 main.1.mp4
-ffmpeg -i main.mp4 -ss 00:02:30 -t 00:02:30 main.2.mp4
-ffmpeg -i main.mp4 -ss 00:05:00 -t 00:02:30 main.3.mp4
-ffmpeg -i main.mp4 -ss 00:07:30 -t 00:02:30 main.4.mp4
+ffmpeg -i main.mp4 -ss 00:00:00 -t 00:02:30 -c copy main.1.mp4
+ffmpeg -i main.mp4 -ss 00:02:30 -t 00:02:30 -c copy main.2.mp4
+ffmpeg -i main.mp4 -ss 00:05:00 -t 00:02:30 -c copy main.3.mp4
+ffmpeg -i main.mp4 -ss 00:07:30 -t 00:02:30 -c copy main.4.mp4
